34360004f0b019a55932eaed7b22580c0f2f77e6
Hiroshi Miura <hirmiura@gmail.com>
2024-11-17 21:54:52 +0900

    fix: 脳死マージを修正

--- src/csharp/Plugin.cs
+++ src/csharp/Plugin.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -16,7 +15,7 @@ public static class MyPluginInfo
 {
     public const string PLUGIN_GUID = "yararezon.yar_craft";
     public const string PLUGIN_NAME = "Yar Craft";
-    public const string PLUGIN_VERSION = "0.2.2";
+    public const string PLUGIN_VERSION = "0.2.3";
 }
 
 
@@ -78,81 +77,3 @@ public class ThingGenPatch
         return __result;
     }
 }
-=======
-﻿using BepInEx;
-using BepInEx.Configuration;
-using BepInEx.Logging;
-using HarmonyLib;
-using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
-using UnityEngine;
-
-namespace YarCraft;
-
-public static class MyPluginInfo
-{
-    public const string PLUGIN_GUID = "yararezon.yar_craft";
-    public const string PLUGIN_NAME = "Yar Craft";
-    public const string PLUGIN_VERSION = "0.2.3";
-}
-
-
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
-{
-    internal static ManualLogSource Loggger;
-    public static Regex Rgx { get; private set; }
-    public static Regex RgxUnderbar { get; private set; }
-    public static string Replacement { get; private set; }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string Replace(string input)
-    {
-        return Plugin.RgxUnderbar.Replace(Plugin.Rgx.Replace(input, Plugin.Replacement), "");
-    }
-
-    private void Awake()
-    {
-        Loggger = base.Logger;
-        // 設定ファイル
-        var executingAssembly = Assembly.GetExecutingAssembly();
-        var directoryName = Path.GetDirectoryName(executingAssembly.Location);
-        var configFile = new ConfigFile(Path.Combine(directoryName, "plugin.cfg"), true);
-        //lang=regex
-        var pattern = configFile.Bind("General", "Pattern", @"^YarCraft_(?<id>.+?)(_(wood|stone|metal|cloth)?)?(_q\d)?$", "正規表現パターン").Value;
-        Plugin.Rgx = new Regex(pattern, RegexOptions.Compiled);
-        Plugin.RgxUnderbar = new Regex("_+$", RegexOptions.Compiled);
-        //lang=regex
-        Plugin.Replacement = configFile.Bind("General", "Replace", @"${id}", "正規表現置換").Value;
-
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");  // BepInExとElin両方のログ
-    }
-
-    private void Start()
-    {
-        Debug.Log(MyPluginInfo.PLUGIN_NAME + " Start");  // Elinのログ
-        var harmony = new Harmony(MyPluginInfo.PLUGIN_NAME);
-        harmony.PatchAll();
-    }
-}
-
-
-[HarmonyPatch(typeof(ThingGen))]
-[HarmonyPatch(nameof(ThingGen._Create))]
-public class ThingGenPatch
-{
-    public static Thing Postfix(Thing __result)
-    {
-        var old_id = __result.id;
-        if (Plugin.Rgx.IsMatch(old_id))
-        {
-            var new_id = Plugin.Replace(old_id);
-            __result.id = new_id;
-            Plugin.Loggger.LogDebug($" ThingGenPatch Postfix: Replace from {old_id} to {new_id}");
-        }
-        return __result;
-    }
-}
->>>>>>> main
