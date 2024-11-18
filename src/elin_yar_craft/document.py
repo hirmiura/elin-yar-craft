a2badaa7ea8c4a9ef575bf2ec9ee9a4454f99a70
Hiroshi Miura <hirmiura@gmail.com>
2024-11-18 23:50:27 +0900

    csvファイルの読込み部分を分離

--- src/elin_yar_craft/document.py
+++ src/elin_yar_craft/document.py
@@ -11,6 +11,8 @@ import sys
 from pathlib import Path
 from typing import Any
 
+type NameDictType = dict[str, list[str]]
+
 
 def procee_args() -> argparse.Namespace:
     """コマンドライン引数を処理する
@@ -34,69 +36,56 @@ def procee_args() -> argparse.Namespace:
 
 def craftable_list(dir: str) -> None:
     assert dir
-    # 日英版
-    csv_dir = Path(dir) / "Yar_Craft"
-    csv_weapon = csv_dir / "EDEFW_Thing_YarCraft_Weapon.csv"
-    csv_armor = csv_dir / "EDEFW_Thing_YarCraft_Armor.csv"
-    csv_accessory = csv_dir / "EDEFW_Thing_YarCraft_Accessory.csv"
-    with open(csv_weapon) as f:
-        reader_weapon = csv.DictReader(f)
-        weapon_list = list(reader_weapon)
-    weapon_name_dict = get_categorized_data(weapon_list, "name")
-    weapon_name_JP_dict = get_categorized_data(weapon_list, "name_JP")  # noqa: N806
-    with open(csv_armor) as f:
-        reader_armor = csv.DictReader(f)
-        armor_list = list(reader_armor)
-    armor_name_dict = get_categorized_data(armor_list, "name")
-    armor_name_JP_dict = get_categorized_data(armor_list, "name_JP")  # noqa: N806
-    with open(csv_accessory) as f:
-        reader_accessory = csv.DictReader(f)
-        accessory_list = list(reader_accessory)
-    accessory_name_dict = get_categorized_data(accessory_list, "name")
-    accessory_name_JP_dict = get_categorized_data(accessory_list, "name_JP")  # noqa: N806
-    # 日英版を出力
+    jp_dir = Path(dir) / "Yar_Craft"
+    cn_dir = Path(dir) / "Yar_Craft_CN"
+    csv_weapon = "EDEFW_Thing_YarCraft_Weapon.csv"
+    csv_armor = "EDEFW_Thing_YarCraft_Armor.csv"
+    csv_accessory = "EDEFW_Thing_YarCraft_Accessory.csv"
     temp_path = Path(dir) / "document_template"
     doc_path = Path(dir) / "docs"
+
+    # 日英版
+    weapon_name_dict, weapon_name_jp_dict = get_name_dict(jp_dir / csv_weapon, True)
+    armor_name_dict, armor_name_jp_dict = get_name_dict(jp_dir / csv_armor, True)
+    accessory_name_dict, accessory_name_jp_dict = get_name_dict(jp_dir / csv_accessory, True)
+    assert weapon_name_jp_dict and armor_name_jp_dict and accessory_name_jp_dict
+    # 日英版を出力
     desc_file = "workshop_desc.txt"
     craftable_jp = gen_craftable_list(
         "近接武器",
         "防具",
         "アクセサリ",
-        weapon_name_JP_dict,
-        armor_name_JP_dict,
-        accessory_name_JP_dict,
+        weapon_name_jp_dict,
+        armor_name_jp_dict,
+        accessory_name_jp_dict,
     )
     gen_workshop_desc(craftable_jp, temp_path / desc_file, doc_path / desc_file)
-    desc_file = "workshop_desc_en.txt"
+    desc_file_en = "workshop_desc_en.txt"
     craftable_jp = gen_craftable_list(
         "Melee", "Armor", "Accessory", weapon_name_dict, armor_name_dict, accessory_name_dict
     )
-    gen_workshop_desc(craftable_jp, temp_path / desc_file, doc_path / desc_file)
+    gen_workshop_desc(craftable_jp, temp_path / desc_file_en, doc_path / desc_file_en)
 
     # 中文版
-    csv_dir = Path(dir) / "Yar_Craft_CN"
-    csv_weapon = csv_dir / "EDEFW_Thing_YarCraft_Weapon.csv"
-    csv_armor = csv_dir / "EDEFW_Thing_YarCraft_Armor.csv"
-    csv_accessory = csv_dir / "EDEFW_Thing_YarCraft_Accessory.csv"
-    with open(csv_weapon) as f:
-        reader_weapon = csv.DictReader(f)
-        weapon_list = list(reader_weapon)
-    weapon_name_dict = get_categorized_data(weapon_list, "name")
-    with open(csv_armor) as f:
-        reader_armor = csv.DictReader(f)
-        armor_list = list(reader_armor)
-    armor_name_dict = get_categorized_data(armor_list, "name")
-    with open(csv_accessory) as f:
-        reader_accessory = csv.DictReader(f)
-        accessory_list = list(reader_accessory)
-    accessory_name_dict = get_categorized_data(accessory_list, "name")
+    weapon_name_dict, _ = get_name_dict(cn_dir / csv_weapon)
+    armor_name_dict, _ = get_name_dict(cn_dir / csv_armor)
+    accessory_name_dict, _ = get_name_dict(cn_dir / csv_accessory)
     # 中文版を出力
-    desc_file = "workshop_desc_cn.txt"
-    craftable_jp = gen_craftable_list(
+    desc_file_cn = "workshop_desc_cn.txt"
+    craftable_cn = gen_craftable_list(
         "近战武器", "裝甲", "装饰", weapon_name_dict, armor_name_dict, accessory_name_dict
     )
-    gen_workshop_desc(craftable_jp, temp_path / desc_file, doc_path / desc_file)
+    gen_workshop_desc(craftable_cn, temp_path / desc_file_cn, doc_path / desc_file_cn)
     return
+
+
+def get_name_dict(path: Path, need_jp: bool = False) -> tuple[NameDictType, NameDictType | None]:
+    with path.open() as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+    name_dict = get_categorized_data(data, "name")
+    name_jp_dict = get_categorized_data(data, "name_JP") if need_jp else None
+    return name_dict, name_jp_dict
 
 
 def get_categorized_data(data: list[dict[str, Any]], key: str) -> dict[str, list[str]]:
